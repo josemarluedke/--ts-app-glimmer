d14208fd8785ca552815da569ce2f89e09fe08ca
Josemar Luedke <josemarluedke@gmail.com>
2019-03-12 13:40:52 -0700

    Add stuff to the component

--- app/components/my-component.ts
+++ app/components/my-component.ts
@@ -1,5 +1,20 @@
 import Component from '@glimmer/component';
 
-export default class MyComponent extends Component {
+interface Args {
+  hello: string;
+}
 
+// The following line does not work
+// export default class MyComponent extends Component<Args> {
+
+export default class MyComponent extends Component {
+  public args!: Args;
+
+  public constructor(owner: never, args: Args) {
+    super(owner, args);
+  }
+
+  didInsertElement() {
+    console.log(this.element)
+  }
 }
